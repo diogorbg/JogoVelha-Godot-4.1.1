eb59d856f61a22eb675a3f97563cc93888b4c339
Diogo RBG <diogorbg@gmail.com>
2023-10-04 15:32:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JogoVelha-Godot-4.1.1
+ Um simples jogo da velha para explorar o potencial dos componentes de UI da Godot 4
---
+++ jogo/button.gd
@@ -0,0 +1,30 @@
+extends Button
+
+const img1 = preload("res://sprites/1.png")
+const img2 = preload("res://sprites/4.png")
+
+@onready var anim = %anim as AnimationPlayer
+@onready var img = %img as TextureRect
+
+@export var cor1: Color
+@export var cor2: Color
+
+static var player1: bool = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_pressed():
+	disabled = true # não poderá mais ser clicado
+	anim.play("click")
+	self_modulate = cor1 if player1 else cor2
+	img.texture = img1 if player1 else img2
+	player1 = !player1
+
+
+# conectar este sinal caso o clique não esteja funcionando no mobile
+#func _on_gui_input(event: InputEvent) -> void:
+#	elif event is InputEventScreenTouch:
+#		if event.pressed: _on_pressed()
